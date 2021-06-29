a14ccfeb9723def5dfd71c20c97b9685d3cbdf17
Katherine Obregon <a20181823@pucp.edu.pe>
2021-06-29 01:01:15 -0500

    pregunta 4 pero falta que muestre mensaje cuando hay error :C

--- .idea/LAB10.js
+++ .idea/LAB10.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mysql = require("mysql2");
+const bodyParser= require("body-parser");
+
 
 let conn = mysql.createConnection({
     host: 'localhost',
@@ -44,6 +46,40 @@ app.get("/productos/get",function(request, response){
         response.json(data);
     });
 });
+
+app.post('/empleados/update', bodyParser.json(), function (req,response){
+    let id = req.body.id;
+    let email = req.body.email;
+    let address = req.body.address;
+    let query ="";
+    let params="";
+    if(address === undefined && email!==undefined){
+         query = "update employees set Email = ? where EmployeeID = ?";
+         params =[email,id];
+    }
+    if(address!==undefined && email===undefined){
+         query = "update employees set Address = ? where EmployeeID = ?";
+         params =[address,id];
+    }
+    if(address!==undefined && email!==undefined){
+         query = "update employees set Address = ? , Email = ? where EmployeeID = ?";
+         params =[address,email,id];
+    }
+    conn.query(query, params, function(error, result){
+        if(error) throw error;
+
+        let returnData ={
+            data:{
+                status:"ok",
+                message: "Employee updated"
+            }
+        }
+        response.json(returnData);
+    });
+
+})
+
+
 
 app.listen(8090, function(){
     console.log("servidor arrancado...");
